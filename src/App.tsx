f2885a639ec0759428b3748d5e8653a12fae95ac
Malin Kyttä <malinkytta@gmail.com>
2023-06-09 23:01:18 +0200

    Added all route paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-// import Container from 'react-bootstrap/Container'
 import { Route, Routes } from 'react-router-dom'
-import './assets/scss/App.scss'
 import HomePage from './pages/HomePage'
 import Navigation from './components/Navigation'
 import NotFound from './pages/NotFound'
@@ -10,6 +8,14 @@ import CharactersPage from './pages/CharactersPage'
 import CharacterPage from './pages/CharacterPage'
 import PlanetsPage from './pages/PlanetsPage'
 import PlanetPage from './pages/PlanetPage'
+import SpeciesPage from './pages/SpeciesPage'
+import StarshipsPage from './pages/StarshipsPage'
+import VehiclesPage from './pages/VehiclesPage'
+import SpeciePage from './pages/SpeciePage'
+import StarShipPage from './pages/StarshipPage'
+import VehiclePage from './pages/VehiclePage'
+
+import './assets/scss/App.scss'
 
 
 const App = () => {
@@ -17,7 +23,6 @@ const App = () => {
   return (
     <div id="App">
       <Navigation />
-      {/* <Container className="py-3"> */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/movies" element={<MoviesPage />} />
@@ -26,13 +31,16 @@ const App = () => {
         <Route path="/characters/:id" element={<CharacterPage />} />
         <Route path="/planets" element={<PlanetsPage />} />
         <Route path="/planets/:id" element={<PlanetPage />} />
-
-
+        <Route path="/species" element={<SpeciesPage />} />
+        <Route path="/species/:id" element={< SpeciePage />} />
+        <Route path="/starships" element={<StarshipsPage />} />
+        <Route path="/starships/:id" element={<StarShipPage />} />
+        <Route path="/vehicles" element={<VehiclesPage />} />
+        <Route path="/vehicles/:id" element={<VehiclePage />} />
 
         <Route path="/*" element={<NotFound />} />
 
       </Routes>
-      {/* </Container> */}
     </div>
   )
 }
