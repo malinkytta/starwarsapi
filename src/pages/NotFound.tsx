478a47e5d63fd7f07188ffd23309efa0b01c86d6
Malin Kyttä <malinkytta@gmail.com>
2023-06-11 23:19:29 +0200

    Added text to 404-page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,13 +1,16 @@
 import Container from "react-bootstrap/Container"
-
+import Button from "react-bootstrap/Button"
 
 const NotFound = () => {
 
     return (
         <div className="not-found">
             <Container>
-                <h1>404 - Not Found.</h1>
-                <h2>Sorry, that page could not be found!</h2>
+
+                <h1>404</h1>
+                <h2>Uh-oh! The droids lost their way in the binary galaxy. This page has joined the dark side. Use your Jedi instincts to navigate back to a glitch-free galaxy far, far away!</h2>
+                <Button href={'/'}
+                    variant="light">Return to the light side</Button>
             </Container>
         </div>
     )
