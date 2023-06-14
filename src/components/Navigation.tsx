a6587374052330d626d22a9fbb41bfd32766bc52
Malin Kyttä <malinkytta@gmail.com>
2023-06-14 14:45:50 +0200

    Fixed navbar so it displays in right order

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,32 +5,31 @@ import { NavLink, Link } from 'react-router-dom'
 
 const Navigation = () => {
     return (
-        <Navbar variant="dark" sticky="top" expand="md" className="justify-content-center">
+        <Navbar variant="dark" sticky="top" expand="md" className="justify-content-center d-flex">
             <Container>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-start">
-                        <Nav.Link as={NavLink} to="/movies">Movies</Nav.Link>
-                        <Nav.Link as={NavLink} to="/characters">Characters</Nav.Link>
-                        <Nav.Link as={NavLink} to="/planets">Planets</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-                <Navbar.Brand as={Link} to="/">
+                <Navbar.Brand as={Link} to="/" className="order-lg-1 order-md-1">
                     <img
                         src="/src/assets/images/star-wars-logo-1002.png"
-                        // width="100"
                         height="60"
                         className="d-inline-block align-top"
                         alt="StarWars Encyclopedia"
                     />
                 </Navbar.Brand>
 
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" className="order-lg-4 order-md-0" />
+                <Navbar.Collapse id="basic-navbar-nav" className="order-lg-0 order-md-0">
+                    <Nav className="ms-start" >
+                        <Nav.Link as={NavLink} to="/movies">Movies</Nav.Link>
+                        <Nav.Link as={NavLink} to="/characters">Characters</Nav.Link>
+                        <Nav.Link as={NavLink} to="/planets">Planets</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+
+                <Navbar.Collapse id="basic-navbar-nav" className="order-lg-2 order-md-2">
+                    <Nav className="ms-md-auto">
                         <Nav.Link as={NavLink} to="/species">Species</Nav.Link>
                         <Nav.Link as={NavLink} to="/starships">Starships</Nav.Link>
                         <Nav.Link as={NavLink} to="/vehicles">Vehicles</Nav.Link>
-
                     </Nav>
                 </Navbar.Collapse>
             </Container>
