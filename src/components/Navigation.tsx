297ebc921e83ed648e830d8c9c56a80c7dac1f1d
Malin Kyttä <malinkytta@gmail.com>
2023-06-15 15:56:56 +0200

    Trying to make navbar disappear on click

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -7,12 +7,13 @@ import Logo from '../assets/images/star-wars-logo-1002.png'
 const Navigation = () => {
 
     return (
-        <Navbar variant="dark" sticky="top" expand="md" className="justify-content-center d-flex">
+        <Navbar variant="dark" sticky="top" expand="md" collapseOnSelect className="justify-content-center d-flex">
             <Container>
                 <Navbar.Brand as={Link} to="/" className="order-lg-1 order-md-1">
                     <img
                         src={Logo}
-                        height="60"
+                        height="60px"
+                        width="123px"
                         className="d-inline-block align-top"
                         alt="StarWars Encyclopedia"
                     />
@@ -22,17 +23,18 @@ const Navigation = () => {
 
                 <Navbar.Collapse id="basic-navbar-nav" className="order-lg-0 order-md-0">
                     <Nav className="ms-start" >
-                        <Nav.Link as={NavLink} to="/movies">Movies</Nav.Link>
-                        <Nav.Link as={NavLink} to="/characters">Characters</Nav.Link>
-                        <Nav.Link as={NavLink} to="/planets">Planets</Nav.Link>
+                        <Nav.Link as={NavLink} eventKey='movies' to="/movies">Movies</Nav.Link>
+                        <Nav.Link as={NavLink} eventKey='characters' to="/characters">Characters</Nav.Link>
+                        <Nav.Link as={NavLink} eventKey='planets' to="/planets">Planets</Nav.Link>
                     </Nav>
+
                 </Navbar.Collapse>
 
                 <Navbar.Collapse id="basic-navbar-nav" className="order-lg-2 order-md-2">
                     <Nav className="ms-auto">
-                        <Nav.Link as={NavLink} to="/species">Species</Nav.Link>
-                        <Nav.Link as={NavLink} to="/starships">Starships</Nav.Link>
-                        <Nav.Link as={NavLink} to="/vehicles">Vehicles</Nav.Link>
+                        <Nav.Link as={NavLink} eventKey='species' to="/species">Species</Nav.Link>
+                        <Nav.Link as={NavLink} eventKey='starships' to="/starships">Starships</Nav.Link>
+                        <Nav.Link as={NavLink} eventKey='vehicles' to="/vehicles">Vehicles</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
