f07d9c02faadd140adfd1bf696ac68c309256299
Malin Kyttä <malinkytta@gmail.com>
2023-06-15 09:56:07 +0200

    Changed href button

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,26 +1,25 @@
 import Container from "react-bootstrap/Container"
 import Button from "react-bootstrap/Button"
-import { useNavigate } from "react-router-dom"
 
 const NotFound = () => {
-    const navigate = useNavigate()
 
     return (
         <div className="not-found">
             <Container>
 
-                <h1>404</h1>
+                <h1 className="pt-4">404</h1>
                 <h2>Uh-oh! The droids lost their way in the binary galaxy. This page has joined the dark side.
                     Use your Jedi instincts to navigate back to a glitch-free galaxy far, far away!</h2>
                 <Button
                     variant="light"
-                    onClick={() => navigate(-1)}>
+                    href={'/'}
+                >
                     Return to safety, young Padawan.
                 </Button>
+
             </Container>
         </div>
     )
-
 }
 
 export default NotFound
